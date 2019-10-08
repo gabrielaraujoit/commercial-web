20f0183e9a92b1ee16fc65acaead814a1fff5461
Gabriel Araújo <gabrielaraujo@MacBook-Air-de-Gabriel.local>
2019-10-07 22:27:42 -0300

    first commit

---
+++ commercial-api/src/main/java/com/algaworks/commercial/CommercialApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.commercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercialApiApplication.class, args);
+	}
+
+}
