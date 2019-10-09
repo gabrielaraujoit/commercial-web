e66ba564fc3ebc37964bc7a3947d59e23ba4a6b8
gabrielaraujo <gabrielaraujo@192.168.0.9>
2019-10-09 20:55:58 -0300

    feat: add new opportunity

--- commercial-api/src/main/java/com/algaworks/commercial/controller/OpportunityController.java
+++ commercial-api/src/main/java/com/algaworks/commercial/controller/OpportunityController.java
@@ -3,11 +3,17 @@ package com.algaworks.commercial.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.commercial.model.Opportunity;
@@ -36,6 +42,12 @@ public class OpportunityController {
 		}
 
 		return ResponseEntity.ok(opportunity.get());
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Opportunity store(@Valid @RequestBody Opportunity opportunity) {
+		return opportunityRepo.save(opportunity);
 	}
 
 }
--- commercial-api/src/main/java/com/algaworks/commercial/model/Opportunity.java
+++ commercial-api/src/main/java/com/algaworks/commercial/model/Opportunity.java
@@ -7,6 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Opportunity {
@@ -15,11 +18,16 @@ public class Opportunity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotEmpty
+	@Size(max = 80)
 	@Column(name = "prospectus_name")
 	private String prospectusName;
 
+	@NotEmpty
+	@Size(max = 200)
 	private String description;
 
+	@Min(0)
 	private BigDecimal value;
 
 	public Long getId() {
